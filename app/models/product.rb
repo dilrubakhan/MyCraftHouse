02e8163ab42b4c7edac8b7cf3c0dbad3bfe1c151
Dilruba Khan <rubatanisa@yahoo.ie>
2020-02-11 22:12:02 +0000

    Design pages layouts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing.jpg"
+  has_attached_file :image, styles: { medium: "150x", thumb: "100x100>" }, default_url: "missing.jpg"
 
 end
